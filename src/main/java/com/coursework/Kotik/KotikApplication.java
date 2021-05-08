5b34b9c3f32cf26dd1d39c32285ab04253d8ad7d
alex-skamander <alex.skamander@gmail.com>
2021-05-08 14:54:05 +0300

    Initial commit

---
+++ src/main/java/com/coursework/Kotik/KotikApplication.java
@@ -0,0 +1,13 @@
+package com.coursework.Kotik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coursework/Kotik/KotikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework.Kotik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
