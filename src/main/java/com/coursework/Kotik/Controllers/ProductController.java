799105f7d678c7e9ce6ae14f5eaf46cc8855926e
alex-skamander <alex.skamander@gmail.com>
2021-05-12 03:01:18 +0300

    Adding products

--- src/main/java/com/coursework/Kotik/Controllers/ProductController.java
+++ src/main/java/com/coursework/Kotik/Controllers/ProductController.java
@@ -1,6 +1,5 @@
 package com.coursework.Kotik.Controllers;
 
-import com.coursework.Kotik.Models.Product;
 import com.coursework.Kotik.Repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,10 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-
 @Controller
-public class CatsController {
+public class ProductController {
 
     @Autowired
     ProductRepository productRepository;
@@ -21,8 +18,8 @@ public class CatsController {
 
         model.addAttribute("products", productRepository.findByType(type));
         System.out.println(productRepository.findAll());
-        model.addAttribute("type", "Всё для " + type);
-        return "cats";
+        model.addAttribute("type", "Everything for " + type);
+        return "product";
     }
 
 }
